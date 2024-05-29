119dc45695dc74dd76326a9a247baf3cfb2054d8
Sven Olafsen <stockburner9@gmail.com>
2024-05-29 00:24:30 -0500

    needer email

---
+++ server.js
@@ -0,0 +1,47 @@
+// import express from "express"
+// import dotenv from "dotenv";
+// import cors from "cors";
+const cors = require('cors')
+const dotenv = require('dotenv')
+const express = require('express')
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+dotenv.config();
+
+app.listen(8000)
+
+
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+);
+console.log("hey")
+
+
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SEND_GRID_API)
+
+app.post("/sendNeederWelcomeEmail", async (req, res) => {
+    console.log(req.body)
+    const userEmail = req.body.email
+const msg = {
+  to: userEmail, // Change to your recipient
+  from: 'john@getfulfil.com', // Change to your verified sender
+  subject: 'Sending with SendGrid is Fun',
+  templateId: 'd-5cceab9612f94ddb8103006d4e5d5e63'
+}
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+
+});
